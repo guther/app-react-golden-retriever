f819722116e6743eab0b4f93059aa1a935bd3364
Alexandre Damasceno <guther.manaus@gmail.com>
2021-02-28 13:13:58 -0400

    Changed folder name

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import "./styles.css"
+
+const Footer = () => {
+    return (
+        <div className="container-footer">
+            <small>&copy; 2021 - Alexandre Damasceno - Atividade 7</small>
+        </div>
+    )
+}
+
+export default Footer;
