e43348c97941ea30d8dc13d31b6aa699c469938f
Alexandre Damasceno <guther.manaus@gmail.com>
2021-02-28 02:41:09 -0400

    Components folder was renamed.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom"
-import Header from "./Components/Header"
-import Footer from "./Components/Footer"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 import Home from "./pages/Home"
 import CondicaoFisica from "./pages/CondicaoFisica"
 import Personalidade from "./pages/Personalidade"
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import CarouselSlider from "../../Components/Carousel"
+import CarouselSlider from "../../components/Carousel"
 import "./styles.css"
 
 const Home = () => {
